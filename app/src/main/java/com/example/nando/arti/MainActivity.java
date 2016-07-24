7b5d0ff18eb0aba46382ac3ca92a3dc9c632020a
Fernando <nando.reina@outlook.es>
2016-07-24 20:13:19 +0200

    minor changes or comments.

--- app/src/main/java/com/example/nando/arti/MainActivity.java
+++ app/src/main/java/com/example/nando/arti/MainActivity.java
@@ -1,16 +1,58 @@
 package com.example.nando.arti;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_ENABLE_BT = 1;
+    private ArrayAdapter<String> _pairedDevicesArrayAdapter;
+    // EXTRA string to send on to mainactivity
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    private BluetoothAdapter _bluetoothAdapter;
+
+    /*
+    OnItemClickListener for any device that appeared at ListView
+     */
+    private AdapterView.OnItemClickListener _deviceClickListener = new AdapterView.OnItemClickListener()
+    {
+        //TODO Metodo necesita arreglos
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+
+            textView1.setText(R.string.connecting);
+
+            // Get the device MAC address, which is the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            // Make an intent to start next activity while taking an extra which is the MAC address.
+            Intent i = new Intent(DeviceAcitivity.this, MainActivity.class);
+            i.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(i);
+        }
+    };
+
+    /*
+    onCreate
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,4 +91,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void InitializeBT()
+    {
+        _bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (_bluetoothAdapter == null)
+        {
+            Toast.makeText(getBaseContext(), R.string.blutooth_not_supported, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            // We have to ask for enable Bluetooth if it is off
+            if (!_bluetoothAdapter.isEnabled())
+            {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            else
+            {
+                Log.d(TAG, "Bluetooth is already working");
+            }
+        }
+
+    }
+
+    private void BT()
+    {
+        // Turn on bluetooth
+        InitializeBT();
+
+        // Initialize array adapter for paired devices
+        _pairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+
+        // Find and set up the ListView for paired devices
+        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
+        pairedListView.setAdapter(_pairedDevicesArrayAdapter);
+        pairedListView.setOnItemClickListener(_deviceClickListener);
+
+        // Get a set of currently paired devices and append to 'pairedDevices'
+        if (_bluetoothAdapter != null)
+        {
+            Set<BluetoothDevice> pairedDevices = _bluetoothAdapter.getBondedDevices();
+        }
+    }
+
+
 }
