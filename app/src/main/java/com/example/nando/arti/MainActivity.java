a71bb4ff3354a3937e210f152892a8fc2d881993
GitSioke <relojeroloco@gmail.com>
2016-07-24 23:17:15 +0200

    Fixes for paired devices

--- app/src/main/java/com/example/nando/arti/MainActivity.java
+++ app/src/main/java/com/example/nando/arti/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.nando.arti;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +18,7 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     public static String EXTRA_DEVICE_ADDRESS = "device_address";
     private BluetoothAdapter _bluetoothAdapter;
 
+
     /*
     OnItemClickListener for any device that appeared at ListView
      */
@@ -37,14 +42,15 @@ public class MainActivity extends AppCompatActivity {
         //TODO Metodo necesita arreglos
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
 
-            textView1.setText(R.string.connecting);
+            TextView textView = (TextView)findViewById(R.id.textView_connecting);
+            textView.setText(R.string.connecting);
 
             // Get the device MAC address, which is the last 17 chars in the View
             String info = ((TextView) v).getText().toString();
             String address = info.substring(info.length() - 17);
 
             // Make an intent to start next activity while taking an extra which is the MAC address.
-            Intent i = new Intent(DeviceAcitivity.this, MainActivity.class);
+            Intent i = new Intent(v.getContext(), DeviceActivity.class);
             i.putExtra(EXTRA_DEVICE_ADDRESS, address);
             startActivity(i);
         }
@@ -68,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        BT();
     }
 
     @Override
@@ -92,6 +100,51 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    /*
+    This method fills with paired devices
+     */
+    private void BT()
+    {
+        // Turn on bluetooth
+        InitializeBT();
+
+        // Initialize array adapter for paired devices
+        _pairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+
+        // Find and set up the ListView for paired devices
+        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
+        pairedListView.setAdapter(_pairedDevicesArrayAdapter);
+        pairedListView.setOnItemClickListener(_deviceClickListener);
+
+        /*// Register for broadcasts when a device is discovered
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(mReceiver, filter);
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);*/
+
+        // Get a set of currently paired devices and append to 'pairedDevices'
+        if (_bluetoothAdapter != null)
+        {
+            Set<BluetoothDevice> pairedDevices = _bluetoothAdapter.getBondedDevices();
+
+            // If there are paired devices, add each one to the ArrayAdapter
+            if (pairedDevices.size() > 0) {
+                findViewById(R.id.tv_paired_devices).setVisibility(View.VISIBLE);
+                for (BluetoothDevice device : pairedDevices) {
+                    _pairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            } else {
+                _pairedDevicesArrayAdapter.add("No paired devices");
+            }
+        }
+    }
+
+    /*
+    This methods starts bluetooth in case it is not ready for use
+     */
     private void InitializeBT()
     {
         _bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -115,25 +168,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void BT()
-    {
-        // Turn on bluetooth
-        InitializeBT();
+    /*private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
 
-        // Initialize array adapter for paired devices
-        _pairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-
-        // Find and set up the ListView for paired devices
-        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
-        pairedListView.setAdapter(_pairedDevicesArrayAdapter);
-        pairedListView.setOnItemClickListener(_deviceClickListener);
-
-        // Get a set of currently paired devices and append to 'pairedDevices'
-        if (_bluetoothAdapter != null)
-        {
-            Set<BluetoothDevice> pairedDevices = _bluetoothAdapter.getBondedDevices();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // If it's already paired, skip it, because it's been listed already
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    newDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+                // When discovery is finished, change the Activity title
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                setProgressBarIndeterminateVisibility(false);
+                setTitle("Select device");
+                if (newDevicesArrayAdapter.getCount() == 0) {
+                    newDevicesArrayAdapter.add("None found");
+                }
+            }
         }
-    }
-
+    };*/
 
 }
