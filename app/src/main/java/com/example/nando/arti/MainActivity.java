cb81635e856a727720d8a472d3b4d0743edbf458
GitSioke <relojeroloco@gmail.com>
2016-08-01 00:06:02 +0200

    Displaying bluetooth devices(found and paired)

--- app/src/main/java/com/example/nando/arti/MainActivity.java
+++ app/src/main/java/com/example/nando/arti/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,6 +23,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int REQUEST_ENABLE_BT = 1;
     private ArrayAdapter<String> _pairedDevicesArrayAdapter;
+    private ArrayAdapter<String> _newDevicesArrayAdapter;
     // EXTRA string to send on to mainactivity
     public static String EXTRA_DEVICE_ADDRESS = "device_address";
     private BluetoothAdapter _bluetoothAdapter;
@@ -37,12 +43,11 @@ public class MainActivity extends AppCompatActivity {
     /*
     OnItemClickListener for any device that appeared at ListView
      */
-    private AdapterView.OnItemClickListener _deviceClickListener = new AdapterView.OnItemClickListener()
-    {
+    private AdapterView.OnItemClickListener _deviceClickListener = new AdapterView.OnItemClickListener() {
         //TODO Metodo necesita arreglos
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
 
-            TextView textView = (TextView)findViewById(R.id.textView_connecting);
+            TextView textView = (TextView) findViewById(R.id.textView_connecting);
             textView.setText(R.string.connecting);
 
             // Get the device MAC address, which is the last 17 chars in the View
@@ -55,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
     };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     /*
     onCreate
@@ -76,6 +86,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         BT();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -104,8 +117,7 @@ public class MainActivity extends AppCompatActivity {
     /*
     This method fills with paired devices
      */
-    private void BT()
-    {
+    private void BT() {
         // Turn on bluetooth
         InitializeBT();
 
@@ -117,17 +129,16 @@ public class MainActivity extends AppCompatActivity {
         pairedListView.setAdapter(_pairedDevicesArrayAdapter);
         pairedListView.setOnItemClickListener(_deviceClickListener);
 
-        /*// Register for broadcasts when a device is discovered
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        this.registerReceiver(mReceiver, filter);
+        // Initialize array adapter for new devices found
+        _newDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
 
-        // Register for broadcasts when discovery has finished
-        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        this.registerReceiver(mReceiver, filter);*/
+        //Find and set up ListView for new devices found
+        ListView newListView = (ListView) findViewById(R.id.new_devices);
+        newListView.setAdapter(_newDevicesArrayAdapter);
+        newListView.setOnItemClickListener(_deviceClickListener);
 
         // Get a set of currently paired devices and append to 'pairedDevices'
-        if (_bluetoothAdapter != null)
-        {
+        if (_bluetoothAdapter != null) {
             Set<BluetoothDevice> pairedDevices = _bluetoothAdapter.getBondedDevices();
 
             // If there are paired devices, add each one to the ArrayAdapter
@@ -145,31 +156,40 @@ public class MainActivity extends AppCompatActivity {
     /*
     This methods starts bluetooth in case it is not ready for use
      */
-    private void InitializeBT()
-    {
+    private void InitializeBT() {
+        System.out.print("Inicializando el bluetooth");
         _bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (_bluetoothAdapter == null)
-        {
+
+        // Register for broadcasts when a device is discovered, when it is started or when it is finished
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
+
+        if (_bluetoothAdapter == null) {
             Toast.makeText(getBaseContext(), R.string.blutooth_not_supported, Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
+        } else {
             // We have to ask for enable Bluetooth if it is off
-            if (!_bluetoothAdapter.isEnabled())
-            {
+            if (!_bluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-            else
-            {
+            } else {
                 Log.d(TAG, "Bluetooth is already working");
             }
+
+            //Now we start to discover BT devices. Cancel previous discovery in case there is any in progress
+            if (_bluetoothAdapter.isDiscovering()) {
+                _bluetoothAdapter.cancelDiscovery();
+            }
+            _bluetoothAdapter.startDiscovery();
         }
 
     }
 
-    /*private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    //BroadcastReceiver where we'll handle broadcast action such as device discovering or discovery ending
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
+
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
@@ -179,17 +199,56 @@ public class MainActivity extends AppCompatActivity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // If it's already paired, skip it, because it's been listed already
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                    newDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                    _newDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 }
                 // When discovery is finished, change the Activity title
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 setProgressBarIndeterminateVisibility(false);
                 setTitle("Select device");
-                if (newDevicesArrayAdapter.getCount() == 0) {
-                    newDevicesArrayAdapter.add("None found");
+                if (_newDevicesArrayAdapter.getCount() == 0) {
+                    _newDevicesArrayAdapter.add("None found");
                 }
             }
         }
-    };*/
+    };
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.nando.arti/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.nando.arti/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
