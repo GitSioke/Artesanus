30c485fa66a6384ad9760619d70069dcd8b733ea
Fernando <nando.reina@outlook.es>
2016-08-07 22:32:09 +0200

    sdfgh

---
+++ app/src/main/java/com/example/nando/arti/NewProcessActivity.java
@@ -0,0 +1,13 @@
+package com.example.nando.arti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_process);
+    }
+}
