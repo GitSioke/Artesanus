b22ccf4bca6b28c673f955460711054ba0d839bf
Fernando <nando.reina@outlook.es>
2016-11-14 22:27:50 +0100

    Cereal fragment
    To control adding cereal process

---
+++ app/src/main/java/nandroid/artesanus/fragments/AddKindFragment.java
@@ -0,0 +1,116 @@
+package nandroid.artesanus.fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import nandroid.artesanus.common.Cereal;
+import nandroid.artesanus.gui.CerealAdapter;
+import nandroid.artesanus.gui.R;
+
+/**
+ * Created by Nando on 10/11/2016.
+ */
+public class AddKindFragment extends DialogFragment
+{
+
+    public static final String LOGTAG = "AddKindFragment";
+    ArrayList<Cereal> dataModels;
+    ListView listView;
+    private static CerealAdapter adapter;
+
+    public interface OnCerealAddedListener{
+        void onCerealAdded(ArrayList<Cereal> cereals);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        dataModels= new ArrayList<>();
+
+        dataModels.add(new Cereal("Apple Pie"));
+        dataModels.add(new Cereal("Banana Bread"));
+        dataModels.add(new Cereal("Cupcake"));
+        dataModels.add(new Cereal("Donut"));
+        dataModels.add(new Cereal("Eclair"));
+        dataModels.add(new Cereal("Froyo"));
+        dataModels.add(new Cereal("Gingerbread"));
+        dataModels.add(new Cereal("Honeycomb"));
+        dataModels.add(new Cereal("Ice Cream Sandwich"));
+        dataModels.add(new Cereal("Jelly Bean"));
+        dataModels.add(new Cereal("Kitkat"));
+        dataModels.add(new Cereal("Lollipop"));
+        dataModels.add(new Cereal("Marshmallow"));
+
+        CerealAdapter adapter = new CerealAdapter(dataModels, getContext());
+
+        final View view  = inflater.inflate( R.layout.dialog_add_cereals, container, false);
+
+        final ListView listView = (ListView)view.findViewById(R.id.dialog_add_cereals_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Cereal dataModel = dataModels.get(position);
+
+                Snackbar.make(view, dataModel.getName() + "\n" + dataModel.getName() + " API: " + dataModel.getAmount(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+            }
+        });
+
+        TextView tvOk = (TextView)view.findViewById(R.id.cereal_ok_button);
+        tvOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ArrayList<Cereal> addedCereals = new ArrayList<Cereal>();
+
+                for (int children = 0; children < listView.getChildCount(); children++)
+                {
+                    View view = listView.getChildAt(children);
+                    EditText ed = (EditText)view.findViewById(R.id.cereal_amount);
+                    int amountToAdd = Integer.parseInt(ed.getText().toString());
+                    if (amountToAdd > 0)
+                    {
+                        Cereal cereal = dataModels.get(children);
+                        cereal.setAmount(amountToAdd);
+                        addedCereals.add(cereal);
+                    }
+                }
+
+                Fragment fragment = getParentFragment();
+                if (fragment instanceof OnCerealAddedListener) {
+                    {
+                        OnCerealAddedListener listener = (OnCerealAddedListener) fragment;
+                        listener.onCerealAdded(addedCereals);
+                    }
+                }
+
+                dismiss();
+            }
+        });
+
+        TextView tvCancel = (TextView)view.findViewById(R.id.cereal_cancel_button);
+        tvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/nandroid/artesanus/gui/CerealAdapter.java
@@ -0,0 +1,96 @@
+package nandroid.artesanus.gui;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import nandroid.artesanus.common.Cereal;
+
+
+public class CerealAdapter extends ArrayAdapter<Cereal> implements View.OnClickListener
+{
+    private static ArrayList<Cereal> dataSet;
+    Context mContext;
+
+    public CerealAdapter(ArrayList<Cereal> data, Context context)
+    {
+        super(context, R.layout.list_row_cereal, data);
+        this.dataSet = data;
+        this.mContext=context;
+    }
+
+    // View lookup cache
+    private static class ViewHolder
+    {
+        TextView txtName;
+        TextView txtAmount;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        int position= (Integer)v.getTag();
+        Object object= getItem(position);
+        Cereal dataModel= (Cereal)object;
+
+        switch (v.getId())
+        {
+            case R.id.cereal_amount:
+                Snackbar.make(v, "Release date " + dataModel.getName(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+                break;
+        }
+    }
+
+    private int lastPosition = -1;
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        // Get the data item for this position
+        Cereal dataModel = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        final ViewHolder viewHolder; // view lookup cache stored in tag
+
+        final View result;
+
+        if (convertView == null) {
+
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_row_cereal, parent, false);
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.txtName);
+            viewHolder.txtAmount = (EditText) convertView.findViewById(R.id.cereal_amount);
+
+            result=convertView;
+
+            convertView.setTag(viewHolder);
+        } else
+        {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+
+        /*Animation animation = AnimationUtils.loadAnimation(mContext, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        result.startAnimation(animation);
+        lastPosition = position;*/
+
+        viewHolder.txtName.setText(dataModel.getName());
+        viewHolder.txtAmount.setText(Integer.toString(dataModel.getAmount()));
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
