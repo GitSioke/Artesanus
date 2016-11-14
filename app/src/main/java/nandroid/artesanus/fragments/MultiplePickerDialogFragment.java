a251910ce41fabc03962595f4739b47138316b2f
Fernando <nando.reina@outlook.es>
2016-11-14 22:29:09 +0100

    NewFragment
    To control tablayout for NewCraftingActivity

---
+++ app/src/main/java/nandroid/artesanus/fragments/MultiplePickerDialogFragment.java
@@ -0,0 +1,7 @@
+package nandroid.artesanus.fragments;
+
+/**
+ * Created by Nando on 06/11/2016.
+ */
+public class MultiplePickerDialogFragment {
+}
---
+++ app/src/main/java/nandroid/artesanus/fragments/NewFragmentPagerAdapter.java
@@ -0,0 +1,36 @@
+package nandroid.artesanus.fragments;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import nandroid.artesanus.common.FragmentFactory;
+
+
+public class NewFragmentPagerAdapter extends FragmentPagerAdapter{
+    final int PAGE_COUNT = 2;
+    private String tabTitles[] = new String[] {"Tipo", "Maceración" };
+    private Context context;
+
+    public NewFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return FragmentFactory.getFragment(position);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
