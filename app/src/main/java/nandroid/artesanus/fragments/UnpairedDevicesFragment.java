e26025cf56018910fb665cc3808c3abf86b9e3e3
Fernando <nando.reina@outlook.es>
2016-12-03 20:06:26 +0100

    UnpairedDevicesFragment can no be used for BluetoothActivity classes

--- app/src/main/java/nandroid/artesanus/fragments/UnpairedDevicesFragment.java
+++ app/src/main/java/nandroid/artesanus/fragments/UnpairedDevicesFragment.java
@@ -4,11 +4,10 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 
-import nandroid.artesanus.gui.MenuActivity;
 import nandroid.artesanus.gui.R;
+import nandroid.artesanus.gui.BluetoothActivity;
 
 /**
  * Created by Nando on 01/11/2016.
@@ -22,12 +21,12 @@ public class UnpairedDevicesFragment extends DialogFragment
         builder.setMessage(R.string.unpaired_fragment_no_pair_device)
                 .setPositiveButton(R.string.unpaired_fragment_pair_devices, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        ((MenuActivity)getActivity()).doPositiveClick();
+                        ((BluetoothActivity)getActivity()).doPositiveClick();
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        ((MenuActivity)getActivity()).doNegativeClick();
+                        ((BluetoothActivity)getActivity()).doNegativeClick();
                     }
                 });
         // Create the AlertDialog object and return it
