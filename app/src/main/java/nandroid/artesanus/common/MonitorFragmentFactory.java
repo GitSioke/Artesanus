40eade63a9e4dad34c3f50c15fd763be6936bcc7
Fernando <nando.reina@outlook.es>
2016-12-03 20:01:28 +0100

    Factory to create fragments for monitoring

---
+++ app/src/main/java/nandroid/artesanus/common/MonitorFragmentFactory.java
@@ -0,0 +1,32 @@
+package nandroid.artesanus.common;
+
+import android.support.v4.app.Fragment;
+
+import nandroid.artesanus.fragments.KindTabFragment;
+import nandroid.artesanus.fragments.MashingTabFragment;
+import nandroid.artesanus.fragments.MonitorBoilTabFragment;
+import nandroid.artesanus.fragments.MonitorFermentationTabFragment;
+import nandroid.artesanus.fragments.MonitorMashingTabFragment;
+
+/**
+ * Created by Nando on 05/11/2016.
+ */
+public class MonitorFragmentFactory {
+
+
+    public final static int MASHING = 0;
+    public final static int HEAT = 1;
+    public final static int FERMENTER = 2;
+
+    public static Fragment getFragment(int type) {
+        switch (type) {
+            case MASHING:
+                return new MonitorMashingTabFragment();
+            case HEAT:
+                return new MonitorBoilTabFragment();
+            case FERMENTER:
+                return new MonitorFermentationTabFragment();
+        }
+        return null;
+    }
+}
