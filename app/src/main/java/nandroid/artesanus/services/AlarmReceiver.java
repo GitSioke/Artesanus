8d76e12826254f7a88de1c89a0d85d14f9359100
Fernando <nando.reina@outlook.es>
2016-12-04 13:18:18 +0100

    Deleted unused import declarations on AlarmReceiver

--- app/src/main/java/nandroid/artesanus/services/AlarmReceiver.java
+++ app/src/main/java/nandroid/artesanus/services/AlarmReceiver.java
@@ -1,16 +1,5 @@
 package nandroid.artesanus.services;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.NotificationCompat;
-import android.widget.Toast;
-
-import nandroid.artesanus.gui.MainActivity;
-import nandroid.artesanus.gui.R;
-
 /**
  * Created by nando on 11/08/2016.
  */
