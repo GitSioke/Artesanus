bfbbb0ea801f14f0799428e07dbb32301d065c19
Fernando <nando.reina@outlook.es>
2017-04-23 19:45:26 +0200

    Android Studio update

--- Android app/app/src/main/java/nandroid/artesanus/fragments/MonitorFermentationTabFragment.java
+++ Android app/app/src/main/java/nandroid/artesanus/fragments/MonitorFermentationTabFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,7 +19,7 @@ import nandroid.artesanus.common.Hop;
 import nandroid.artesanus.gui.R;
 
 /**
- * Created by Nando on 05/11/2016.
+ * This class handles the Monitor tab fragment for Fermentation process
  */
 public class MonitorFermentationTabFragment extends Fragment
 {
@@ -32,6 +33,18 @@ public class MonitorFermentationTabFragment extends Fragment
                              Bundle savedInstanceState)
     {
         View view  = inflater.inflate( R.layout.fragment_monitoring_fermenter, container, false);
+        final ImageView imgDensityButton = (ImageView)view.findViewById(R.id.monitor_add_density_icon);
+        final EditText densityEditText = (EditText)view.findViewById(R.id.monitor_density_edit_text);
+        imgDensityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                densityEditText.setVisibility(View.VISIBLE);
+                densityEditText.onEndBatchEdit();
+                imgDensityButton.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
         return view;
     }
 
