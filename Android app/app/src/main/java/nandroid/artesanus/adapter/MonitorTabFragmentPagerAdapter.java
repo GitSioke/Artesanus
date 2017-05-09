d7a44e9294d20ee351e304b45ba00020c290f73d
Fernando <nando.reina@outlook.es>
2017-05-09 03:24:31 +0200

    Added string arrays for Tab fragment names

--- Android app/app/src/main/java/nandroid/artesanus/adapter/MonitorTabFragmentPagerAdapter.java
+++ Android app/app/src/main/java/nandroid/artesanus/adapter/MonitorTabFragmentPagerAdapter.java
@@ -5,17 +5,21 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.Arrays;
+
 import nandroid.artesanus.factories.MonitorFragmentFactory;
+import nandroid.artesanus.gui.R;
 
 
 public class MonitorTabFragmentPagerAdapter extends FragmentPagerAdapter{
     final int PAGE_COUNT = 3;
-    private String tabTitles[] = new String[] {"Maceracion","Coccion", "Fermentacion"};
+    private String tabTitles[];
     private Context context;
 
     public MonitorTabFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
+        tabTitles = this.context.getResources().getStringArray(R.array.string_array_processes);
     }
 
     @Override
--- Android app/app/src/main/java/nandroid/artesanus/adapter/TabFragmentPagerAdapter.java
+++ Android app/app/src/main/java/nandroid/artesanus/adapter/TabFragmentPagerAdapter.java
@@ -6,16 +6,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import nandroid.artesanus.factories.FragmentFactory;
+import nandroid.artesanus.gui.R;
 
 
 public class TabFragmentPagerAdapter extends FragmentPagerAdapter{
     final int PAGE_COUNT = 2;
-    private String tabTitles[] = new String[] {"Tipo", "Maceración" };
+    private String tabTitles[];
     private Context context;
 
     public TabFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
+        tabTitles = this.context.getResources().getStringArray(R.array.string_array_new_crafting_tabs);
     }
 
     @Override
